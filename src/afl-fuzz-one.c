2742bfab05dc660181b4bbf233d3049cfbe72fc5
Dan Blackwell <dblackwell@viru10.viru10>
2022-05-18 14:59:43 +0100

    Seem to fix issue better

--- src/afl-fuzz-one.c
+++ src/afl-fuzz-one.c
@@ -7997,7 +7997,11 @@ havoc_stage:
 
             } else {
 
-              copy_len = choose_block_len(afl, new_secret_len - 1);
+              if (new_secret_len > 0) {
+                copy_len = choose_block_len(afl, new_secret_len - 1);
+              } else {
+                copy_len = 0;
+              }
               if (copy_len > temp_combined_len) copy_len = temp_combined_len;
               copy_from = rand_below(afl, new_combined_len - copy_len + 1);
               copy_to = rand_below(afl, temp_combined_len - copy_len + 1);
